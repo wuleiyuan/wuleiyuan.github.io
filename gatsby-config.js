78d3156e4586c4fee5001556b01985e2ccd9c9e7
LeiYuan wu <leiyuanwu@LeiYuandeMacBook-Air.local>
2022-07-25 15:57:38 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
+  pathPrefix: '/Running_Page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'WuLeiYuan Running Page',
     siteUrl: 'https://yihong.run',
