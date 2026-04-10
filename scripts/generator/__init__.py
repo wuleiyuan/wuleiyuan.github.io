eed267cc90fe0dbf19a4f0b49795cf7c56cdf599
wuleiyuan <wuly@youyuan.com>
2026-04-10 11:04:36 +0800

    fix: filter activities with distance > 2km

--- scripts/generator/__init__.py
+++ scripts/generator/__init__.py
@@ -96,7 +96,7 @@ class Generator:
     def load(self):
         activities = (
             self.session.query(Activity)
-            .filter(Activity.distance > 0.1)
+            .filter(Activity.distance > 2000)
             .order_by(Activity.start_date_local.desc())
         )
         activity_list = []
